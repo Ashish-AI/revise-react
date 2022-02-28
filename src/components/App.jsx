cc019e3262ef251b53923d0dc56fa3c71255632c
Ashish Chaubey <ashishchaubey866@gmail.com>
2022-02-28 22:58:04 +0530

    mapping data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import details from "./details";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+
+function createNote(detail) {
+  return (
+    <Note
+      title={detail.title}
+      subtitle={detail.subtitle}
+      key={detail.id}
+    ></Note>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Note title="Title 1" subtitle="This is subtitle 1"></Note>
-      <Note title="Title 2" subtitle="This is subtitle 2"></Note>
+      <p> {details.map(createNote)}</p>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/details.js
@@ -0,0 +1,13 @@
+const details = [
+  {
+    id: 1,
+    title: "This is title 1",
+    subtitle: "This is subtitle 1",
+  },
+  {
+    id: 1,
+    title: "This is title 2",
+    subtitle: "This is subtitle 2",
+  },
+];
+export default details;
